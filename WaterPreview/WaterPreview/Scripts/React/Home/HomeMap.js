38158818caad72c298af8aee20a3077458754159
Kate605690919 <229026@whut.edu.cn>
2017-12-14 10:29:34 +0800

    地图图例

--- WaterPreview/WaterPreview/Scripts/React/Home/HomeMap.js
+++ WaterPreview/WaterPreview/Scripts/React/Home/HomeMap.js
@@ -12,7 +12,16 @@
     }
     render() {
         return (
-            <div id="homeMap" style={{ marginBottom: '10px' }}></div>
+            <div className="map">
+                <div id="homeMap" style={{ marginBottom: '10px' }}></div>
+                <div id="legend">
+                    <ul>
+                        <li><i></i><span>流量计</span></li>
+                        <li><i></i><span>压力计</span></li>
+                        <li><i></i><span>水质计</span></li>
+                    </ul>
+                </div>
+            </div>
         );
     }
 }
--- WaterPreview/WaterPreview/Scripts/React/Home/index.js
+++ WaterPreview/WaterPreview/Scripts/React/Home/index.js
@@ -1,12 +1,7 @@
-﻿let { Button } = antd;
-class Home extends React.Component {
+﻿class Home extends React.Component {
     constructor(props) {
         super(props);
         this.state = { date: '' };
-    }
-    handleChange(date) {
-        message.info('您选择的日期是： ' + date.toString());
-        this.setState({ date });
     }
     render() {
         return (
@@ -21,59 +16,3 @@ class Home extends React.Component {
         );
     }
 }
-
-//render() {
-//    let { ViewLog, PMViewLog, QMViewLog } = this.state;
-//    return (
-//        <div className="homeBody" style={{ width: '100%' }}>
-//            <div className="commonData">
-//                <div className="commonDevice">
-//                    <div style={{ color: 'rgb(158, 158, 158)', marginBottom: '10px', fontSize: '14px' }}># 常用流量计</div>
-//                    <CardListItem chart={false} bigH={
-//                        ViewLog ? (ViewLog[0] ? { header: ViewLog[0].flowmeter.FM_Description, content: parseInt(ViewLog[0].lastday_flow).toFixed(2) } : { header: '暂无', content: '暂无' }) : { header: '加载中...', content: '加载中...' }
-//                    } smallH={
-//                        { header: '昨日总流量', content: ViewLog ? (ViewLog[0] ? ViewLog[0].lastday_flow_proportion : '暂无') : '加载中...' }
-//                    } />
-//                    <CardListItem chart={false} bigH={
-//                        ViewLog ? (ViewLog[1] ? { header: ViewLog[1].flowmeter.FM_Description, content: parseInt(ViewLog[1].lastday_flow).toFixed(2) } : { header: '暂无', content: '暂无' }) : { header: '加载中...', content: '加载中...' }
-//                    } smallH={
-//                        { header: '昨日总流量', content: ViewLog ? (ViewLog[1] ? ViewLog[1].lastday_flow_proportion : '暂无') : '加载中...' }
-//                    } />
-//                    <CardListItem chart={false} bigH={
-//                        ViewLog ? (ViewLog[2] ? { header: ViewLog[2].flowmeter.FM_Description, content: parseInt(ViewLog[2].lastday_flow).toFixed(2) } : { header: '暂无', content: '暂无' }) : { header: '加载中...', content: '加载中...' }
-//                    } smallH={
-//                        { header: '昨日总流量', content: ViewLog ? (ViewLog[2] ? ViewLog[2].lastday_flow_proportion : '暂无') : '加载中...' }
-//                    } />
-//                </div>
-//                <div className="commonDevice">
-//                    <div style={{ color: 'rgb(158, 158, 158)', marginBottom: '10px', fontSize: '14px' }}># 常用水压计</div>
-//                    <CardListItem chart={false} bigH={
-//                        PMViewLog ? (PMViewLog[0] ? { header: PMViewLog[0].pressuremeter.PM_Description, content: parseInt(PMViewLog[0].lastday_pressure).toFixed(2) } : { header: '暂无', content: '暂无' }) : { header: '加载中...', content: '加载中...' }
-//                    } smallH={
-//                        { header: '昨日总流量', content: PMViewLog ? (PMViewLog[0] ? PMViewLog[0].lastday_pressure_proportion : '暂无') : '加载中...' }
-//                    } />
-//                    <CardListItem chart={false} bigH={
-//                        PMViewLog ? (PMViewLog[1] ? { header: PMViewLog[1].pressuremeter.PM_Description, content: parseInt(PMViewLog[1].lastday_pressure).toFixed(2) } : { header: '暂无', content: '暂无' }) : { header: '加载中...', content: '加载中...' }
-//                    } smallH={
-//                        { header: '昨日总流量', content: PMViewLog ? (PMViewLog[1] ? PMViewLog[1].lastday_pressure_proportion : '暂无') : '加载中...' }
-//                    } />
-//                </div>
-//                <div className="commonDevice">
-//                    <div style={{ color: 'rgb(158, 158, 158)', marginBottom: '10px', fontSize: '14px' }}># 常用水质计</div>
-//                    <CardListItem chart={false} bigH={
-//                        PMViewLog ? (PMViewLog[0] ? { header: PMViewLog[0].pressuremeter.PM_Description, content: parseInt(PMViewLog[0].lastday_pressure).toFixed(2) } : { header: '暂无', content: '暂无' }) : { header: '加载中...', content: '加载中...' }
-//                    } smallH={
-//                        { header: '昨日总流量', content: PMViewLog ? (PMViewLog[0] ? PMViewLog[0].lastday_pressure_proportion : '暂无') : '加载中...' }
-//                    } />
-//                    <CardListItem chart={false} bigH={
-//                        PMViewLog ? (PMViewLog[1] ? { header: PMViewLog[1].pressuremeter.PM_Description, content: parseInt(PMViewLog[1].lastday_pressure).toFixed(2) } : { header: '暂无', content: '暂无' }) : { header: '加载中...', content: '加载中...' }
-//                    } smallH={
-//                        { header: '昨日总流量', content: PMViewLog ? (PMViewLog[1] ? PMViewLog[1].lastday_pressure_proportion : '暂无') : '加载中...' }
-//                    } />
-//                </div>
-//                <div id="homeMap" style={{ marginBottom: '10px' }}></div>
-//            </div>
-//            <Footer />
-//        </div>
-//    );
-//}
