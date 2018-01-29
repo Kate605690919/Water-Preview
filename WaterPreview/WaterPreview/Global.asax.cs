a02a7d08878a57578549d9f656bebea3982a23c6
jerliu <1093329945@qq.com>
2018-01-29 15:11:12 +0800

    修改跨域设置方式

--- WaterPreview/WaterPreview/Global.asax.cs
+++ WaterPreview/WaterPreview/Global.asax.cs
@@ -51,7 +51,7 @@ namespace WaterPreview
             dependencyResolver.Register<IQualityMeterStatusService, QualityMeterStatusService>();
             dependencyResolver.Register<IExceptionService, ExceptionService>();
 
-            GlobalConfiguration.Configuration.EnableCors();   
+            //GlobalConfiguration.Configuration.EnableCors();   
             GlobalConfiguration.Configuration.DependencyResolver = dependencyResolver;
            
         }
